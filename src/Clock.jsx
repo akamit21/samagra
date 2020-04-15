a50f7af563286bcb80ce173a71d79dd04fda49f1
Amit Kumar <akamit21@outlook.com>
2020-04-15 17:24:54 +0530

    Clock added

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from "react";
 const Clock = () => {
   const [time, setTime] = useState(Date.now());
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setTime(Date.now());
-  //   }, 1000);
-  // });
+  useEffect(() => {
+    setInterval(() => {
+      setTime(Date.now());
+    }, 1000);
+  });
   return <h1>{time}</h1>;
 };
 
