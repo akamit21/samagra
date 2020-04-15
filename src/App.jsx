85a1d4eff3ca86e5458db86cf7280802fd8bd691
Amit Kumar <akamit21@outlook.com>
2020-04-15 19:16:33 +0530

    Component added

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import {
   fetchAllPosts,
   fetchAllData,
 } from "./redux/actions/fetchAction";
-import { Container, Jumbotron, Row, Col, Table, Button } from "react-bootstrap";
+import { Container, Jumbotron, Row, Col } from "react-bootstrap";
 import Clock from "./Clock";
+import DataTable from "./DataTable";
+import APIButton from "./APIButton";
 
 class App extends Component {
   componentDidMount() {
@@ -18,153 +20,34 @@ class App extends Component {
   }
 
   render() {
+    const { comment, photo, todo, post } = this.props;
     return (
       <Container className="my-5">
         <Jumbotron>
           <Row className="text-center">
-            <Col>Test App</Col>
-          </Row>
-          <hr />
-          <Row>
-            <Col md={6}>
-              <Table striped bordered hover>
-                <tbody>
-                  <tr>
-                    <td>Start</td>
-                    <td>{this.props.commentsStartTime}</td>
-                  </tr>
-                  <tr>
-                    <td>End</td>
-                    <td>{this.props.commentsEndTime}</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-            <Col md={6}>
-              <Table striped bordered hover>
-                <tbody>
-                  <tr>
-                    <td>Start</td>
-                    <td>{this.props.photosStartTime}</td>
-                  </tr>
-                  <tr>
-                    <td>End</td>
-                    <td>{this.props.photosEndTime}</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-            <Col md={6}>
-              <Table striped bordered hover>
-                <tbody>
-                  <tr>
-                    <td>Start</td>
-                    <td>{this.props.todosStartTime}</td>
-                  </tr>
-                  <tr>
-                    <td>End</td>
-                    <td>{this.props.todosEndTime}</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-            <Col md={6}>
-              <Table striped bordered hover>
-                <tbody>
-                  <tr>
-                    <td>Start</td>
-                    <td>{this.props.postsStartTime}</td>
-                  </tr>
-                  <tr>
-                    <td>End</td>
-                    <td>{this.props.postsEndTime}</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                  </tr>
-                </tbody>
-              </Table>
+            <Col>
+              <h1>Test App</h1>
             </Col>
           </Row>
           <hr />
           <Row>
-            <Col md={6}>
-              <Button
-                type="button"
-                className="my-2"
-                variant="outline-dark"
-                size="lg"
-                block
-                onClick={() => this.props.getComments()}
-              >
-                Get Comments
-              </Button>
-            </Col>
-            <Col md={6}>
-              <Button
-                type="button"
-                className="my-2"
-                variant="outline-dark"
-                size="lg"
-                block
-                onClick={() => this.props.getPhotos()}
-              >
-                Get Photos
-              </Button>
-            </Col>
-            <Col md={6}>
-              <Button
-                type="button"
-                className="my-2"
-                variant="outline-dark"
-                size="lg"
-                block
-                onClick={() => this.props.getTodos()}
-              >
-                Get Todos
-              </Button>
-            </Col>
-            <Col md={6}>
-              <Button
-                type="button"
-                className="my-2"
-                variant="outline-dark"
-                size="lg"
-                block
-                onClick={() => this.props.getPosts()}
-              >
-                Get Posts
-              </Button>
-            </Col>
+            <DataTable data={comment} />
+            <DataTable data={photo} />
+            <DataTable data={todo} />
+            <DataTable data={post} />
+          </Row>
+          <hr />
+          <Row>
+            <APIButton
+              name="Get Comments"
+              handler={() => this.props.getComments()}
+            />
+            <APIButton
+              name="Get Photos"
+              handler={() => this.props.getPhotos()}
+            />
+            <APIButton name="Get Todos" handler={() => this.props.getTodos()} />
+            <APIButton name="Get Post" handler={() => this.props.getPosts()} />
           </Row>
           <hr />
           <Row className="text-center">
@@ -179,14 +62,10 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  commentsStartTime: state.commentsStartTime,
-  commentsEndTime: state.commentsEndTime,
-  photosStartTime: state.photosStartTime,
-  photosEndTime: state.photosEndTime,
-  todosStartTime: state.todosStartTime,
-  todosEndTime: state.todosEndTime,
-  postsStartTime: state.postsStartTime,
-  postsEndTime: state.postsEndTime,
+  comment: state.comment,
+  photo: state.photo,
+  todo: state.todo,
+  post: state.post,
 });
 const mapDispatchToProps = (dispatch) => ({
   getAllData: () => dispatch(fetchAllData()),
